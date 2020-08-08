b064df440ca7cfd6187defeb7348cffef23936fb
Ankush <ankushbisht72@gmail.com>
2020-08-09 00:00:29 +0530

    ch(deploy) : Setup deploy to heroku

--- aiden/settings.py
+++ aiden/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,3 +135,6 @@ STATIC_ROOT = os.path.join(BASE_DIR,"assets")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+django_heroku.settings(locals())
+
